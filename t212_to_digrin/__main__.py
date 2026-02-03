4f4100bc073c4f750fd2070ba063a26896b16c85
Lukin Kratas <lukin.kratas@seznam.cz>
2026-02-03 12:22:51 +0100

    add cocstrings

--- t212_to_digrin/__main__.py
+++ t212_to_digrin/__main__.py
@@ -10,22 +10,27 @@ from .utils import log_func
 logger = logging.getLogger(__name__)
 configure_logging()
 
-@log_func(logger.info) # unable to log with input
+
+@log_func(logger.info)  # unable to log with input
 def get_input_dt() -> date:
+    """Get input year_month for the report export."""
     prev_month: date = date.today() - relativedelta(months=1)  # day does not matter
 
     print('Reporting Year Month in "YYYY-mm" format:')
     print(f'Or confirm default "{prev_month.strftime("%Y-%m")}" by ENTER.')
     inp = input()
 
-    if inp == '':
+    if inp == "":
         return prev_month
 
-    return datetime.strptime(inp, '%Y-%m')
+    return datetime.strptime(inp, "%Y-%m")
+
 
 def main() -> None:
+    """Logic for CLI entrypoint."""
     input_dt = get_input_dt()
     run(input_dt)
+
 
 if __name__ == "__main__":
     main()
