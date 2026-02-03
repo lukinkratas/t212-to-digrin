9de91102508f7c82b2f00bdb4accd6e412a1f2de
Lukin Kratas <lukin.kratas@seznam.cz>
2026-02-03 12:57:00 +0100

    add wrapper func

--- t212_to_digrin/aws.py
+++ t212_to_digrin/aws.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Any, Callable
 
 import boto3
 from botocore.exceptions import ClientError
@@ -12,41 +13,37 @@ secrets_manager = boto3.client("secretsmanager")
 s3 = boto3.client("s3")
 
 
-@log_func(logger.debug)
-def get_secret(secret_name: str) -> str:
-    """Fetch secret key-value pairs from Secrets Manager Service."""
+def _request(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
+    """Wrap boto function in try-except block."""
     try:
-        response = secrets_manager.get_secret_value(SecretId=secret_name)
+        response = func(*args, **kwargs)
 
     except ClientError as e:
         logging.error(e)
         raise e
 
+    return response
+
+
+@log_func(logger.debug)
+def get_secret(secret_name: str) -> str:
+    """Fetch secret key-value pairs from Secrets Manager Service."""
+    response = _request(secrets_manager.get_secret_value, SecretId=secret_name)
     return json.loads(response["SecretString"])
 
 
 @log_func(logger.debug)
 def upload_file(fileobj: bytes, bucket: str, key: str) -> None:
     """Upload file bytes to S3 service."""
-    try:
-        s3.upload_fileobj(Fileobj=fileobj, Bucket=bucket, Key=key)
-
-    except ClientError as e:
-        logging.error(e)
-        raise e
+    _request(s3.upload_fileobj, Fileobj=fileobj, Bucket=bucket, Key=key)
 
 
 @log_func(logger.debug)
 def get_download_url(bucket: str, key: str) -> str:
     """Generate a download url for file stored in S3 service."""
-    try:
-        url = s3.generate_presigned_url(
-            "get_object",
-            Params={"Bucket": bucket, "Key": key},
-            ExpiresIn=120,  # 2min
-        )
-    except ClientError as e:
-        logging.error(e)
-        raise e
-
-    return url
+    return _request(
+        s3.generate_presigned_url,
+        "get_object",
+        Params={"Bucket": bucket, "Key": key},
+        ExpiresIn=120,  # 2min
+    )
