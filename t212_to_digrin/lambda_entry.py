0574e680ca7764b68518d26ba584fd0893b7d086
Lukin Kratas <lukin.kratas@seznam.cz>
2026-02-03 12:23:23 +0100

    lint, add docstrings

--- t212_to_digrin/lambda_entry.py
+++ t212_to_digrin/lambda_entry.py
@@ -10,11 +10,13 @@ from .main import run
 logger = logging.getLogger(__name__)
 configure_logging()
 
+
 def lambda_handler(event, context):
+    """Logic for lambda entrypoint."""
     print(event)
     print(context)
 
     prev_month: date = date.today() - relativedelta(months=1)  # day does not matter
     run(prev_month)
 
-    return {'statusCode': 200, 'body': json.dumps('Success!')}
+    return {"statusCode": 200, "body": json.dumps("Success!")}
