838da014b33438c56c81ffc1c6dba76ec5b851e7
Lukin Kratas <lukin.kratas@seznam.cz>
2026-02-03 12:23:02 +0100

    add docstrings

--- t212_to_digrin/aws.py
+++ t212_to_digrin/aws.py
@@ -1,5 +1,6 @@
 import json
 import logging
+
 import boto3
 from botocore.exceptions import ClientError
 
@@ -7,12 +8,13 @@ from .utils import log_func
 
 logger = logging.getLogger(__name__)
 
-secrets_manager = boto3.client('secretsmanager')
-s3 = boto3.client('s3')
+secrets_manager = boto3.client("secretsmanager")
+s3 = boto3.client("s3")
+
 
 @log_func(logger.debug)
 def get_secret(secret_name: str) -> str:
-
+    """Fetch secret key-value pairs from Secrets Manager Service."""
     try:
         response = secrets_manager.get_secret_value(SecretId=secret_name)
 
@@ -20,34 +22,31 @@ def get_secret(secret_name: str) -> str:
         logging.error(e)
         raise e
 
-    return json.loads(response['SecretString'])
+    return json.loads(response["SecretString"])
+
 
 @log_func(logger.debug)
-def upload_file(fileobj: bytes, bucket: str, key:str) -> None:
-
+def upload_file(fileobj: bytes, bucket: str, key: str) -> None:
+    """Upload file bytes to S3 service."""
     try:
-        response = s3.upload_fileobj(
-            Fileobj=fileobj,
-            Bucket=bucket,
-            Key=key,
-        )
+        s3.upload_fileobj(Fileobj=fileobj, Bucket=bucket, Key=key)
 
     except ClientError as e:
         logging.error(e)
         raise e
 
+
 @log_func(logger.debug)
 def get_download_url(bucket: str, key: str) -> str:
-
+    """Generate a download url for file stored in S3 service."""
     try:
         url = s3.generate_presigned_url(
-            'get_object',
-            Params={'Bucket': bucket, 'Key': key},
-            ExpiresIn=120, # 2min
+            "get_object",
+            Params={"Bucket": bucket, "Key": key},
+            ExpiresIn=120,  # 2min
         )
     except ClientError as e:
         logging.error(e)
         raise e
 
     return url
-
