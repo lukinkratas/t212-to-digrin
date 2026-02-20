6d050259e48ac1418f4ca01b730bab5a9631eb6b
Lukin Kratas <47260673+lukinkratas@users.noreply.github.com>
2026-02-20 09:14:09 +0100

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,18 @@ set -e
 [[ -d build ]] && rm -rvf build && echo "Deleting build dir." || echo "OK: build dir does not exist."
 # create build dir
 mkdir -v build
-# install pkgs into build dir
-python3 -m pip install pyproject.toml --target build
+
+# # delete requirements if exists
+# [[ -f requirements.txt ]] && rm requirements.txt && echo "Deleting requirements.txt file." || echo "OK: requirements.txt file does not exist."
+# # export requirements
+# uv export --format requirements-txt > requirements.txt
+# # install pkgs into build dir
+# uv pip install --requirements requirements.txt --target build
+
 # copy source module into build dir
-cp -r t212_to_digrin build/
+cp -r t212_to_digrin/* build/
 
 # delete zip if exists
-[[ -f lambda.zip ]] && rm lambda.zip && echo "Deleting zip file." || echo "OK: zip file does not exist."
+[[ -f lambda.zip ]] && rm lambda.zip && echo "Deleting lambda.zip file." || echo "OK: lambda.zip file does not exist."
 # create new zip in a subshell
 (cd build && zip -r ../lambda.zip . -x \*__pycache__)
