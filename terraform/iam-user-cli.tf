0c5090f462dfc9cefcffc186bfc5214c9ee981ea
Lukin Kratas <47260673+lukinkratas@users.noreply.github.com>
2026-02-21 21:22:15 +0100

    move send email policy from cli use to lambda

--- terraform/iam-user-cli.tf
+++ terraform/iam-user-cli.tf
@@ -3,21 +3,6 @@ resource "aws_iam_user" "cli_user" {
   tags = aws_servicecatalogappregistry_application.app.application_tag
 }
 
-resource "aws_iam_user_policy" "s3_get" {
-  name = "S3Read${local.policy_suffix}"
-  user = aws_iam_user.cli_user.name
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.bucket.arn}/*"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_user_policy_attachment" "cli_user_s3_put" {
   user       = aws_iam_user.cli_user.name
   policy_arn = aws_iam_policy.s3_put.arn
